0131cc329cc1bd6fd58cdf00c2359ee015f058fd
Will Foran <willforan@gmail.com>
2014-11-04 15:14:56 -0500

    centeralize settings

---
+++ popSettings.m
@@ -0,0 +1,29 @@
+function set=popSettings()
+
+persistent s;
+if isempty(s)
+     KbName('UnifyKeyNames');
+     
+     %% number of trials
+     s.ntrials.quest    = 200;
+     s.ntrials.test     = 96;
+     s.ntrials.congonly = 96;
+
+     %% screen info
+     s.screen.res       = [1600 1200];
+     s.screen.bgColor=256/2*[1 1 1];
+
+     %% keys -- what keys can we use
+     s.keys = KbName({'1!','0)','escape'});
+     
+     %% timing
+     s.order={'ITI','Prp','Cue','Fbk'};
+     s.timing.ITI=1.0;
+     s.timing.Prp=0.5;
+     s.timing.Cue=0.5;
+     s.timing.Fbk=0.5;
+end
+
+set = s;
+
+end
